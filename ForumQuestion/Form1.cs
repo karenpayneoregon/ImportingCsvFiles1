f3684ff903827d4cf72909150cda3c1691783f40
Karen Payne <paynekaren@comcast.net>
2019-11-18 17:47:25 -0800

    Added example from a forum question which shows handling uneven rows in a delimited file using the same techniques as done in the code sample prior.

---
+++ ForumQuestion/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ForumQuestion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += Form1_Shown;
+        }
+
+        private void Form1_Shown(object sender, System.EventArgs e)
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, 
+                "Import1.txt");
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    Console.WriteLine($"{line.ProcessSpacings()}");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ForumQuestion/readme.md
@@ -0,0 +1,7 @@
+﻿# Reading uneven columns
+
+This example came from a forum question while writing the article. The text file has
+- Uneven columns
+- Extra spaces and tabs mixed together.
+
+End result is a perfect read of each row.
